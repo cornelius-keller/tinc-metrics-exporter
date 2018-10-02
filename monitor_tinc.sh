3a1c8799dfed2b88c5a90600325e94fa4e0f858a
Paul Kremer <pkremer@spurious.biz>
2018-10-02 12:06:15 +0200

    Refinement for race conditions

--- monitor_tinc.sh
+++ monitor_tinc.sh
@@ -23,23 +23,26 @@ OUTFILE="${PROM_STATS_DIR}/tinc.prom"
 while true; do
   echo "" > "${OUTFILE}.$$"
   for nodefile in hosts/*; do
-    if [ ! -f "${nodefile}" ]; then
-      continue
-    fi
-    node=$(awk '/Subnet/ {gsub(/0\/[0-9]+/, "1"); print $3}' ${nodefile})
-    prom_node=$(echo ${node} | tr '.' '_')
-    response_time=$(ping -c 1 ${node} | awk '/time=/ {gsub(/time=/, ""); print $7}')
+    # Race condition: these files may disappear after entering this loop!
+    # We mitigate by testing if we were able to parse the node out.
+    node=$(awk '/Subnet/ {gsub(/0\/[0-9]+/, "1"); print $3}' "${nodefile}")
+    [[ -z "$node" ]] && continue
+    prom_node=$(echo "${node}" | tr '.' '_')
+    response_time=$(ping -c 1 "${node}" | awk '/time=/ {gsub(/time=/, ""); print $7}')
     if [ "$response_time" != "" ]; then
       UP=1
     else
       UP=0
       response_time="-1"
     fi
-    echo "# HELP tinc_node_${prom_node}_up reachability of ${node} via the network overlay" >> "${OUTFILE}.$$"
-    echo "tinc_node_${prom_node}_up{hostname=\"${HOSTNAME}\"} ${UP}" >> "${OUTFILE}.$$"
-    echo "# HELP tinc_node_${prom_node}_response_time response time  of ${node} via the network overlay" >> "${OUTFILE}.$$"
-    echo "tinc_node_${prom_node}_response_time{hostname=\"${HOSTNAME}\"} ${response_time}" >> "${OUTFILE}.$$"
-    tostatsd "tinc.node_response_time:${response_time}|g|#node:${node}" ${STATSD_HOST:-127.0.0.1} ${STATSD_PORT:-8125}
+    # use sub-shell for redirected output to outfile:
+    {
+        echo "# HELP tinc_node_${prom_node}_up reachability of ${node} via the network overlay"
+        echo "tinc_node_${prom_node}_up{hostname=\"${HOSTNAME}\"} ${UP}"
+        echo "# HELP tinc_node_${prom_node}_response_time response time  of ${node} via the network overlay"
+        echo "tinc_node_${prom_node}_response_time{hostname=\"${HOSTNAME}\"} ${response_time}"
+    } >> "${OUTFILE}.$$"
+    tostatsd "tinc.node_response_time:${response_time}|g|#node:${node}" "${STATSD_HOST:-127.0.0.1}" "${STATSD_PORT:-8125}"
   done
   mv "${OUTFILE}.$$" "${OUTFILE}"
   sleep 60
